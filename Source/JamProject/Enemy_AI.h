24dd0772d4210b9a6057b2e84419948faef22422
big-green <72560034+big-green@users.noreply.github.com>
2022-01-12 15:07:52 +0000

    Partial Update

---
+++ Source/JamProject/Enemy_AI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Enemy_AI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JAMPROJECT_API AEnemy_AI : public AAIController
+{
+	GENERATED_BODY()
+	
+};
